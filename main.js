790e71e1fbfbdc4477f8b8d4f5395e106dabe7ff
vmustafa <vmustafa@outlook.com>
2016-04-28 17:25:09 +0530

    Added files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/quote.jpg') {
+      myImage.setAttribute ('src','images/quote1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/quote.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
